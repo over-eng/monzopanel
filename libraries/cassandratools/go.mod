a8b9e44f23d05d2cae678f45a593d85267612a84
John Newman <john@beeper.com>
2024-11-13 10:51:07 +0000

    add cassandratools lib to standardise migrations

---
+++ libraries/cassandratools/go.mod
@@ -0,0 +1,17 @@
+module github.com/over-eng/monzopanel/libraries/cassandratools
+
+go 1.22.7
+
+require (
+	github.com/gocql/gocql v1.7.0
+	github.com/rs/zerolog v1.33.0
+)
+
+require (
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+)
---
+++ libraries/cassandratools/migrations.go
@@ -0,0 +1,140 @@
+package cassandratools
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/gocql/gocql"
+	"github.com/rs/zerolog/log"
+)
+
+type Migration struct {
+	Up      func(*gocql.Session) error
+	Version string
+	Down    func(*gocql.Session) error
+}
+
+func RunMigrations(ctx context.Context, session *gocql.Session, migrations []Migration) error {
+	log := log.With().Ctx(ctx).Logger()
+
+	err := assertMigrationsInOrder(migrations)
+	if err != nil {
+		log.Err(err).Msg("migrations are not in order")
+		return err
+	}
+
+	current, err := getCurrentVersion(session)
+	if err != nil {
+		log.Err(err).Msg("could not determine the current migration version")
+		return err
+	}
+
+	if current == getLatestVersion(migrations) {
+		log.Info().Msg("no migrations run, already on latest version")
+	}
+
+	for _, migration := range migrations {
+		if migration.Version > current {
+			log.Info().Str("version", migration.Version).Msg("running migration")
+			err = runMigration(session, migration)
+			if err != nil {
+				log.Err(err).Msg("failed to apply migration")
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func runMigration(session *gocql.Session, migration Migration) error {
+	err := migration.Up(session)
+	if err != nil {
+		return err
+	}
+
+	cql := "INSERT INTO migrations (version, applied_at) VALUES (?, ?)"
+	err = session.Query(cql, migration.Version, time.Now()).Exec()
+	if err != nil {
+		return errors.Join(errors.New("failed to store current migration version in database"), err)
+	}
+	return nil
+}
+
+func assertMigrationsInOrder(migrations []Migration) error {
+	for i, migration := range migrations {
+		if i == 0 {
+			continue
+		}
+
+		if migration.Version <= migrations[i-1].Version {
+			return errors.New("migration invalid, versions must be alphabetical")
+		}
+	}
+	return nil
+}
+
+func getLatestVersion(migrations []Migration) string {
+	return migrations[len(migrations)-1].Version
+}
+
+func getCurrentVersion(session *gocql.Session) (string, error) {
+	exists, err := migrationTableExists(session)
+	if err != nil {
+		return "", err
+	}
+	if !exists {
+		err = createMigrationTable(session)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	// Note: the default ordering ensures we have the latest version
+	cql := `
+		SELECT version
+		FROM migrations
+		LIMIT 1
+	`
+	var version string
+	err = session.Query(cql).Scan(&version)
+	if err != nil && !errors.Is(err, gocql.ErrNotFound) {
+		return "", err
+	}
+
+	return version, nil
+}
+
+func migrationTableExists(session *gocql.Session) (bool, error) {
+	cql := `
+        SELECT COUNT(*) 
+        FROM system_schema.tables 
+        WHERE keyspace_name = 'events' AND table_name = 'migrations'
+    `
+
+	var count int
+	err := session.Query(cql).Scan(&count)
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
+
+func createMigrationTable(session *gocql.Session) error {
+	cql := `
+		CREATE TABLE IF NOT EXISTS migrations (
+			version text,
+			applied_at timestamp,
+			PRIMARY KEY (applied_at, version)
+		) WITH CLUSTERING ORDER BY (version DESC)
+    `
+
+	err := session.Query(cql).Exec()
+	if err != nil {
+		return fmt.Errorf("failed to create migrations table: %v", err)
+	}
+
+	return nil
+}
