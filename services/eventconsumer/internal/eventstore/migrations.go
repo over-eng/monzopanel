e04642be43c1b8aca5c51192784c8fa38f9f83fa
John Newman <john@over-engineering.co.uk>
2024-11-22 23:22:53 +0000

    add events by hour counter table

--- services/eventconsumer/internal/eventstore/migrations.go
+++ services/eventconsumer/internal/eventstore/migrations.go
@@ -7,6 +7,7 @@ import (
 
 var migrations = []cassandratools.Migration{
 	_01_create_events,
+	_02_create_events_by_hour_counter,
 }
 
 var _01_create_events = cassandratools.Migration{
@@ -22,12 +23,32 @@ var _01_create_events = cassandratools.Migration{
 				client_timestamp timestamp,
 				created_at timestamp,
 				loaded_at timestamp,
-				PRIMARY KEY (created_at, team_id, event, distinct_id, id)
-			)
+				PRIMARY KEY (team_id, distinct_id, created_at, id)
+			) WITH CLUSTERING ORDER BY (distinct_id ASC, created_at DESC)
 		`
 		return session.Query(cql).Exec()
 	},
 	Down: func(session *gocql.Session) error {
 		return session.Query("DROP TABLE IF EXISTS events").Exec()
+	},
+}
+
+var _02_create_events_by_hour_counter = cassandratools.Migration{
+	Version: "0002_create_events_by_hour_counter",
+	Up: func(session *gocql.Session) error {
+		cql := `
+			CREATE TABLE events_by_hour_counter (
+				team_id text,
+				distinct_id text,
+				bucket_hour timestamp,
+				event text,
+				event_count counter,
+				PRIMARY KEY ((team_id, bucket_hour), event, distinct_id)
+			)
+		`
+		return session.Query(cql).Exec()
+	},
+	Down: func(session *gocql.Session) error {
+		return session.Query("DROP TABLE IF EXISTS events_by_hour_counter").Exec()
 	},
 }
